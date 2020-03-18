fd84959af9dd0119e7b06e5394242a5c866aef1a
Quinn Webber <webber.quinn@gmail.com>
2020-03-18 11:01:04 -0230

    Update 2-NN-dist.R

---
+++ code/2-NN-dist.R
@@ -0,0 +1,15 @@
+
+
+### Packages ----
+libs <- c('data.table', 'ggplot2', 'spatsoc')
+lapply(libs, require, character.only = TRUE)
+
+
+## load data
+DT <- readRDS("output/1-caribou-all.RDS")
+
+
+dist <- edge_dist(DT = DT, id = 'IDYr', coords = c('EASTING', 'NORTHING'),
+                   timegroup = 'timegroup', threshold = 50, returnDist = TRUE, 
+                   fillNA = FALSE,
+                   splitBy = c("Year"))
