e18115aa468cf418a9d0a6e349845237776105a1
Quinn Webber <webber.quinn@gmail.com>
2020-03-18 11:02:51 -0230

    NN

--- code/2-NN-dist.R
+++ code/2-NN-dist.R
@@ -8,7 +8,7 @@ lapply(libs, require, character.only = TRUE)
 ## load data
 DT <- readRDS("output/1-caribou-all.RDS")
 
-
+## calculate nearest neighbour distance
 dist <- edge_dist(DT = DT, id = 'IDYr', coords = c('EASTING', 'NORTHING'),
                    timegroup = 'timegroup', threshold = 50, returnDist = TRUE, 
                    fillNA = FALSE,
