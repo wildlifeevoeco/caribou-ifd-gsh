78edbca5d3c2d9cf382cd7db6220506ea6d070d1
Quinn Webber <webber.quinn@gmail.com>
2020-03-23 14:37:35 -0230

    Update 2-NN-dist.R

--- code/2-NN-dist.R
+++ code/2-NN-dist.R
@@ -1,7 +1,7 @@
 
 
 ### Packages ----
-libs <- c('data.table', 'ggplot2', 'spatsoc')
+libs <- c('data.table', 'ggplot2', 'spatsoc', 'gridExtra')
 lapply(libs, require, character.only = TRUE)
 
 
@@ -50,3 +50,38 @@ ggplot(DT2) +
   geom_point(aes(EASTING, NORTHING, color = agg), alpha = 0.25) +
   facet_wrap(~Year)
 
+png("graphics/HistNNdist.png", height = 3000, width = 6000, res = 600, units = "px")
+aa <-ggplot(distNN) +
+  geom_histogram(aes(distance/1000, fill = factor(Year))) +
+  ylab("Count") +
+  xlab("Nearest neighbour distnace (km)") +
+  ggtitle('A)') +
+  theme(legend.position = 'none', 
+        axis.title = element_text(size = 12, color = 'black'),
+        axis.text = element_text(size = 10, color = 'black'),
+        plot.title=element_text(size = 12, hjust=0),
+        axis.line = element_line(colour = "black"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        strip.background = element_rect(colour="black", size = 1, fill = "white"),
+        strip.text = element_text(size = 12)) +
+  facet_wrap(~Year)
+
+bb <- ggplot(distNN[distance < 500]) +
+  geom_histogram(aes(distance, fill = factor(Year))) +
+  ylab("Count") +
+  xlab("Nearest neighbour distnace (m)") +
+  ggtitle('B)') +
+  theme(legend.position = c(0.85, 0.25),  
+        legend.title = element_blank(),
+        axis.title = element_text(size = 12, color = 'black'),
+        axis.text = element_text(size = 10, color = 'black'),
+        plot.title=element_text(size = 12, hjust=0),
+        axis.line = element_line(colour = "black"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        strip.background = element_rect(colour="black", size = 1, fill = "white"),
+        strip.text = element_text(size = 12)) +
+  facet_wrap(~Year)
+grid.arrange(aa,bb, ncol = 2)
+dev.off()
