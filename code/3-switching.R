50e399920e180748baad5d9dc9a45467219db6fa
Quinn Webber <webber.quinn@gmail.com>
2020-04-03 08:57:01 -0230

    switch summary stats

--- code/3-switching.R
+++ code/3-switching.R
@@ -1,11 +1,8 @@
 
 library(data.table)
 
-
+## load data
 avg_season <- fread("data/avg_season_NNdist.csv")
-
-
-avg_season$Year<-factor(avg_season$Year, labels=c("2009","2010","2011","2012","2013"))
 
 avg_season$CalvingGround2<- ifelse(avg_season$CalvingGround=="On",0,1)
 
@@ -14,10 +11,14 @@ avg_season$CalvingGround2<- ifelse(avg_season$CalvingGround=="On",0,1)
 # 0 = no switch, 1 = switching event
 
 avg_season$ID <- as.factor(avg_season$ID)
-library(data.table)
 
 avg_season$Lag1 <- ave(avg_season$CalvingGround2, avg_season$ID, 
                        FUN= function(x) c(NA, x[-length(x)] )  )
-View(avg_season)
 
 avg_season$switch <- abs(avg_season$CalvingGround2 - avg_season$Lag1)
+
+switch <- avg_season[, c("ID", "switch", "Year")]
+
+switch <- switch[!is.na(switch)]
+
+
