31bebf5f79eea744830ab79b1dd862321aee9f48
Quinn Webber <webber.quinn@gmail.com>
2020-05-07 15:27:17 -0230

    Update Fig2.R

--- code/Fig2.R
+++ code/Fig2.R
@@ -9,7 +9,7 @@ lapply(libs, require, character.only = TRUE)
 #for the broken stick
 yravglocs <- read.csv('data/figures/Fig2/ranks.csv')
 # for the voronoi polygons
-raw_all_NNdist <- read.csv("data/figures/Fig2/raw_all_NNdist.csv")
+raw_all_NNdist <- read.csv("data/locs/raw_all_NNdist.csv")
 
 #### Broken stick plots ----
 
